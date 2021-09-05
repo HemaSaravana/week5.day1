1cca467c45dd39e482721e48fd41dbe3f972dbb5
HemaSaravana <88588594+HemaSaravana@users.noreply.github.com>
2021-09-05 13:21:47 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,44 @@
+package testcase;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class CreateLead extends BaseClass {
+@Test
+	public void runCreateLead (){
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hema");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("M");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("9935435");
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+}
+
+
+
+
+
+
